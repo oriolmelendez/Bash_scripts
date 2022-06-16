d93c9dec1193b478c5d39babe2752741a5d0c8ad
oriolmelendez <60789135+oriolmelendez@users.noreply.github.com>
2022-06-16 20:15:19 +0200

    Add files via upload

---
+++ up_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+STR=$(ps aux | grep docker-compose)
+SUB='docker-compose up'
+if [[ "$STR" == *"$SUB"* ]]; then
+  echo "El docker està funcionant"
+  sleep 3
+  exit 0
+fi
+
+cd ~/home/folder_compose_file
+docker-compose up
+
+if [ $? -eq 1 ]
+then
+	echo "Docker are running."
+	echo "Starting to down dockers."
+	docker stop $(docker ps -q)
+	docker-compose up
+	exit 0
+fi
